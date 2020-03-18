35b99b22daa402e2a4e271be032a8b1de3a0de7b
Anton Lapin <antonlapin@Antons-MacBook-Pro.local>
2020-03-18 14:40:37 +0300

    beta 1.1

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -10,8 +10,18 @@ socketio = SocketIO(app)
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    return render_template('session.html')
+
+
+def messageReceived(methods=['GET', 'POST']):
+    print("message was received!!!")
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=["GET", "POST"]):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
